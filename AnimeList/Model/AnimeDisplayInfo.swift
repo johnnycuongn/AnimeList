96976b90dc3367ac4b210b45a4a9f7bd73d4399d
johnnycuongn <cuongdn2001@gmail.com>
2020-09-15 20:52:12 +1000

    Add AnimeDisplayInfo Change some models into class

---
+++ AnimeList/Model/AnimeDisplayInfo.swift
@@ -0,0 +1,41 @@
+//
+//  AnimeDisplayInfo.swift
+//  AnimeList
+//
+//  Created by Johnny on 15/9/20.
+//  Copyright © 2020 Johnny. All rights reserved.
+//
+
+import Foundation
+
+class AnimeDisplayInfo: Decodable {
+    var malID: Int
+    var imageURL: URL
+    var title: String
+    var type: AnimeType
+    var episodes: Int?
+    var members: Int
+    var score: Double
+    
+    enum CodingKeys: String, CodingKey {
+        case malID = "mal_id"
+        case title
+        case imageURL = "image_url"
+        case type
+        case episodes
+        case members
+        case score
+    }
+    
+    required init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        
+        malID = try container.decode(Int.self, forKey: .malID)
+        title = try container.decode(String.self, forKey: .title)
+        imageURL = try container.decode(URL.self, forKey: .imageURL)
+        type = try container.decode(AnimeType.self, forKey: .type)
+        episodes = try? container.decode(Int.self, forKey: .episodes)
+        members = try container.decode(Int.self, forKey: .members)
+        score = try container.decode(Double.self, forKey: .score)
+    }
+}
