fb2c1580e95056c36e44212dc9c730a6c04c40ae
johnnycuongn <cuongdn2001@gmail.com>
2020-12-07 19:43:41 +1100

    Add Genre.swift

---
+++ AnimeList/Domain/Entities/Common&Properties/Genre.swift
@@ -0,0 +1,17 @@
+//
+//  Genre.swift
+//  AnimeList
+//
+//  Created by Johnny on 7/12/20.
+//  Copyright © 2020 Johnny. All rights reserved.
+//
+
+import Foundation
+
+enum Genre: Int, CaseIterable {
+    case Action = 1
+    case Adventure
+    case Cars
+    case Comedy
+    case Dementia, Demons, Mystery, Drama, Ecchi, Fantasy, Game, Hentai, Historical, Horror, Kids, Magic, Mecha, Music, Parody, Samurai, Romance, School, Scifi, Shoujo, ShoujiAi, Shounen, ShounenAi, Space, Sports, SuperPower, Vampire, Yaoi, Yuri, Harem, SliceOfLife, SuperNatural, Military, Police, Psychological, Thriller, Seinen, Josei
+}
