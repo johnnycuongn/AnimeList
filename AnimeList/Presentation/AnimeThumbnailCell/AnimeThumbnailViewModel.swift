30b8df79d681522bcd02aaacdd2b816a8ce6ef9b
johnnycuongn <cuongdn2001@gmail.com>
2020-12-09 10:32:36 +1100

    update DefaultAnimeThumnailViewModel

--- AnimeList/Presentation/AnimeThumbnailCell/AnimeThumbnailViewModel.swift
+++ AnimeList/Presentation/AnimeThumbnailCell/AnimeThumbnailViewModel.swift
@@ -19,3 +19,41 @@ protocol AnimeThumbnailViewModel {
     var type: String { get }
     var episode: String { get }
 }
+
+class DefaultAnimeThumbnailViewModel: AnimeThumbnailViewModel {
+    var id: Int
+    
+    var imageURL: URL
+    var title: String
+    
+    var score: String
+    var members: String
+    
+    var type: String
+    var episode: String
+    
+    init(animeThumbnail: AnimeThumbnail) {
+        self.id = animeThumbnail.malID
+        
+        if let urlPath = animeThumbnail.imageURL {
+            self.imageURL = urlPath
+        } else {
+            self.imageURL = URL(string: "")!
+        }
+        
+        self.title = animeThumbnail.title
+        
+        if animeThumbnail.score == 0 {
+            self.score = "-" }
+        else {
+            self.score = validateLabel(animeThumbnail.score) }
+        
+        self.members = validateLabel(animeThumbnail.members)
+        
+        self.type = validateLabel(animeThumbnail.type?.rawValue, return: .none)
+        
+        self.episode = validateLabel(animeThumbnail.episodes)
+    }
+    
+    
+}
