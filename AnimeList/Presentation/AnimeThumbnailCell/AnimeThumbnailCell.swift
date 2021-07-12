7f46e4ac10e5977f70516f7735ad0a2510681d8e
Johnny Nguyen <cuongdn2001@gmail.com>
2021-07-12 19:11:53 +1000

    Refactor

--- AnimeList/Presentation/AnimeThumbnailCell/AnimeThumbnailCell.swift
+++ AnimeList/Presentation/AnimeThumbnailCell/AnimeThumbnailCell.swift
@@ -44,43 +44,11 @@ class AnimeThumbnailCell: UICollectionViewCell {
         
         animeTitleView.layer.cornerRadius = rankView.layer.cornerRadius
     }
-    
-    func config(with animeInfo: AnimeThumbnailDTO, rank: Int? = nil) {
-        if let imageURL = animeInfo.imageURL {
-            self.animeImageView.loadUsingCache(with: imageURL)
-        }
-        
-        if rank != nil {
-            self.rankLabel.text = String(rank!)
-        } else {
-            self.rankView.isHidden = true
-            self.rankLabel.isHidden = true
-        }
-        self.animeTitleLabel.text = animeInfo.title
-        
-        if animeInfo.score == 0 {
-            self.scoreLabel.text = "-"
-        }
-        else {
-            self.scoreLabel.text = validateLabel(animeInfo.score)
-        }
-        
-        self.memberLabel.text = validateLabel(animeInfo.members)
-        
-        self.typeEpisodeLabel.text = {
-//            guard animeInfo.type != nil else { return "" }
-//            guard animeInfo.episodes != nil else { return "\(animeInfo.type!.rawValue)" }
-            
-            let type = validateLabel(animeInfo.type?.rawValue, return: .none)
-            let episode = validateLabel(animeInfo.episodes)
-            
-            return "\(type)(\(episode))"
-        }()
-    }
-    
+
     func fill(with viewModel: AnimeThumbnailViewModel, rank: String? = nil) {
         if let imageURL = viewModel.imageURL {
             self.animeImageView.loadUsingCache(with: imageURL)
+            setNeedsLayout()
         }
         
         if rank != nil {
